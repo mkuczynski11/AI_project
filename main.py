3d14855a41dd57d8103b00310a49319045543951
Piotr Woliński <piotr.wolinski.2017@outlook.com>
2021-03-20 15:17:59 +0100

    Checked dataset

--- main.py
+++ main.py
@@ -10,8 +10,17 @@ from typing import Dict, Text
 import functions as f
 
 def main():
+    ds = tfds.load('movielens/latest-small-ratings', split='train')
 
-    return
+    ds = ds.take(20).cache()
+    print(ds)
+
+    for example in ds:
+        print(list(example.keys()))
+        title = example['movie_title']
+        rating = example['user_rating']
+        print(f'{title} - {rating}')
+
 
 if __name__ == '__main__':
     main()
