74220011056da749b6f3a0eb2f8db3c5f86bf542
mkuczynski11 <59289239+mkuczynski11@users.noreply.github.com>
2021-03-27 12:24:30 +0100

    Recommendation by genre completed + comments cleared

--- functions.py
+++ functions.py
@@ -5,18 +5,11 @@ import pandas as pd
 #x - DataFrame to analyze
 #top - float value from 0 to 1 that determines percentage of top movies in terms of votes casted
 def top_movies_general(x:pd.DataFrame, top:float) -> pd.DataFrame:
-    #Computing mean ov vote average
     C = float(x['vote_average'].mean())
-    #Computing top 5% movies in terms of vote casted
     m = x['vote_count'].quantile(1 - top)
-
-    #Creating topMovies dataframe which contains movies that are in top 5% in terms of vote casted
+    
     topMovies = x[(x['vote_count'] >= m)]
-    #Creating new column for Weighted Rating
-    #axis = 1 -> applying function on each row
     topMovies['WR'] = topMovies.apply(weighted_rating, args=(m,C,), axis=1)
-    #amount - determines how many top movies we want to list
-    #Sorting values by Weighted Rating in top movies list and getting only specified amount of them
     topMovies = topMovies.sort_values("WR", ascending=False)
     return topMovies
 
@@ -24,11 +17,12 @@ def top_movies_general(x:pd.DataFrame, top:float) -> pd.DataFrame:
 #x - DataFrame to analyze
 #top - float value from 0 to 1 that determines percentage of top movies in terms of votes casted
 #genre - string which represents genre that should be listed
-def top_movies_genre(x:pd.DataFrame, top:float, genre:str) -> pd.DataFrame:
-    #Creating topMovies dataframe which contains movies that contains desired genre
-    
-    #return top_movies_general(topMovies,top)
-    return
+def top_movies_genre(df:pd.DataFrame, top:float, genre:str) -> pd.DataFrame:
+    s = df.apply(lambda x: pd.Series(x['genres']),axis=1).stack().reset_index(level=1, drop=True)
+    s.name = 'genre'
+    gen_md = df.drop('genres', axis=1).join(s)
+    topMovies = gen_md[gen_md['genre'] == genre]
+    return top_movies_general(topMovies,top)
 
 def weighted_rating(x:pd.DataFrame, m:float, C:float):
     v = x['vote_count']
--- main.py
+++ main.py
@@ -6,39 +6,33 @@ import numpy as np
 def main():
     #----------------Data prep----------------#
     #read csv
-    movies_data = pd.read_csv("movies_metadata.csv", dtype='unicode')
+    movies_data = pd.read_csv("movies_metadata.csv")
     #select columns, that we are interested in
     movies_data = movies_data[["genres", "vote_count", "vote_average","release_date","title"]]
     #print(movies_data)
 
     #First we need to fix genres column in a way that it contains arrays of each genre name
-
-    #if value of genres in NA/NaN, then replace it with []
-    #then we apply literal_eval to this column, wchich makes our value a(we will consider list only)
-    #then we take each value x from our column and if x is list, than we take all 'name' elements from ach dictionary in list
-    #if x is not list, than we simply replace any data in column with empty list
     movies_data['genres'] = movies_data['genres'].fillna('[]').apply(literal_eval).apply(lambda x: [i['name'] for i in x] if isinstance(x, list) else [])
+    #print(movies_data)
 
     #Next step is to fix vote_count column and vote_average column
-
-    #Getting the votes count for movies as ints
-    #fillna(0) -> filling NA/NaN values with 0
     movies_data['vote_count'] = movies_data['vote_count'].fillna(0).astype(int)
-    #Getting the votes average for movies as ints
-    movies_data['vote_average'] = movies_data['vote_average'].astype(float)
     #print(movies_data['vote_count'])
-    #print(movies_data['vote_average'])
 
     #----------------Data prep----------------#
 
     #----------------Raw data recommending----------------#
     #First part is recommender based on raw movie ratings
     top_general = top_movies_general(movies_data, 0.01)
-    print(top_general)
+    #print(top_general)
 
     top_by_genre = top_movies_genre(movies_data, 0.05, 'Romance')
-    print(top_by_genre)
+    #print(top_by_genre)
     #----------------Raw data recommending----------------#
+
+    #----------------Content based----------------#
+
+    #----------------Content based----------------#
     return
 
 if __name__ == '__main__':
