c7b5e73880827b4d910f3a91cf318669dbd5dccf
Piotr Woliński <piotr.wolinski.2017@outlook.com>
2021-03-27 13:10:43 +0100

    Recommending by year + refactoring

--- functions.py
+++ functions.py
@@ -4,27 +4,40 @@ import pandas as pd
 #returns top movies based only on user votes
 #x - DataFrame to analyze
 #top - float value from 0 to 1 that determines percentage of top movies in terms of votes casted
-def top_movies_general(x:pd.DataFrame, top:float) -> pd.DataFrame:
-    C = float(x['vote_average'].mean())
-    m = x['vote_count'].quantile(1 - top)
+def top_movies_general(df:pd.DataFrame, top:float) -> pd.DataFrame:
+    C = float(df['vote_average'].mean())
+    m = df['vote_count'].quantile(1 - top)
     
-    topMovies = x[(x['vote_count'] >= m)]
-    topMovies['WR'] = topMovies.apply(weighted_rating, args=(m,C,), axis=1)
-    topMovies = topMovies.sort_values("WR", ascending=False)
-    return topMovies
+    top_movies = df[(df['vote_count'] >= m)]
+    top_movies['WR'] = top_movies.apply(weighted_rating, args=(m,C,), axis=1)
+    top_movies = top_movies.sort_values("WR", ascending=False)
+    return top_movies
 
 #returns top movies with specified genre based only on user votes
 #x - DataFrame to analyze
 #top - float value from 0 to 1 that determines percentage of top movies in terms of votes casted
 #genre - string which represents genre that should be listed
-def top_movies_genre(df:pd.DataFrame, top:float, genre:str) -> pd.DataFrame:
-    s = df.apply(lambda x: pd.Series(x['genres']),axis=1).stack().reset_index(level=1, drop=True)
+def top_movies_by_genre(df:pd.DataFrame, top:float, genre:str) -> pd.DataFrame:
+    s = df.apply(lambda df: pd.Series(df['genres']),axis=1).stack().reset_index(level=1, drop=True)
     s.name = 'genre'
     gen_md = df.drop('genres', axis=1).join(s)
-    topMovies = gen_md[gen_md['genre'] == genre]
-    return top_movies_general(topMovies,top)
+    top_movies = gen_md[gen_md['genre'] == genre]
+    return top_movies_general(top_movies,top)
 
-def weighted_rating(x:pd.DataFrame, m:float, C:float):
-    v = x['vote_count']
-    R = x['vote_average']
+def weighted_rating(df:pd.DataFrame, m:float, C:float) -> float:
+    v = df['vote_count']
+    R = df['vote_average']
     return (((v/(v+m)) * R) + ((m/(m+v)) * C))
+
+
+def top_movies_by_year(df: pd.DataFrame, top: float, year: int, below: bool) -> pd.DataFrame:
+    top_movies = df
+    
+    if below:
+        top_movies = top_movies[(top_movies['release_date'] <= year)]
+    else:  
+        top_movies = top_movies[(top_movies['release_date'] >= year)]
+
+    return top_movies_general(top_movies, top)
+
+        
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from ast import literal_eval
-from functions import top_movies_general, top_movies_genre, weighted_rating
+from functions import top_movies_general, top_movies_by_genre, weighted_rating, top_movies_by_year
 import pandas as pd
 import numpy as np
 
@@ -19,20 +19,33 @@ def main():
     movies_data['vote_count'] = movies_data['vote_count'].fillna(0).astype(int)
     #print(movies_data['vote_count'])
 
+    # Changing release_data to year only
+    movies_data = movies_data[movies_data['release_date'].notna()]
+    movies_data = movies_data.reset_index(drop=True)
+    movies_data['release_date'] = movies_data['release_date'].apply(lambda date: date[:4:])
+    movies_data['release_date'] = pd.to_numeric(movies_data['release_date'])
+    # print(movies_data['release_date'])
     #----------------Data prep----------------#
 
     #----------------Raw data recommending----------------#
     #First part is recommender based on raw movie ratings
-    top_general = top_movies_general(movies_data, 0.01)
+    # top_general = top_movies_general(movies_data, 0.01)
     #print(top_general)
 
-    top_by_genre = top_movies_genre(movies_data, 0.05, 'Romance')
+    # top_by_genre = top_movies_by_genre(movies_data, 0.05, 'Romance')
     #print(top_by_genre)
+
+    top_by_year = top_movies_by_year(movies_data, 0.01, 2013, below=False)
+    print(top_by_year)
     #----------------Raw data recommending----------------#
 
     #----------------Content based----------------#
 
     #----------------Content based----------------#
+
+    #----------------Colaborative based----------------#
+
+    #----------------Colaborative based----------------#
     return
 
 if __name__ == '__main__':
